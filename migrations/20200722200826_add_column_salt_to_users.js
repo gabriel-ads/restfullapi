835ed645766b5231b8a98a89a3be49ac8f26cb21
Gabriel <gabriel.alves450@hotmail.com>
2020-07-24 21:58:17 -0300

    Change to development

--- migrations/20200722200826_add_column_salt_to_users.js
+++ migrations/20200722200826_add_column_salt_to_users.js
@@ -11,6 +11,6 @@ exports.up = function (knex) {
 
 exports.down = function (knex) {
     return knex.schema.table(tableName, table => {
-        tableName.dropColumn(columnName)
+        table.dropColumn(columnName)
     })
 };
